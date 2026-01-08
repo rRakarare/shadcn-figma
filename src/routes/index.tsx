fe114821d33826c142f085460dd691c8f36d99b8
rRakarare <rRene333@googlemail.com>
2026-01-08 14:18:42 +0100

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { createFileRoute } from '@tanstack/react-router'
+import {
+  Zap,
+  Server,
+  Route as RouteIcon,
+  Shield,
+  Waves,
+  Sparkles,
+} from 'lucide-react'
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+
+  return (
+   
+    <div>Hello</div>
+  )
+}

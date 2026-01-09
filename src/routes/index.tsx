d1a5be1c72667a1f02d6d4b77663d204490d88d9
rRakarare <rRene333@googlemail.com>
2026-01-09 12:27:55 +0100

    installed some components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
-import {
-  Zap,
-  Server,
-  Route as RouteIcon,
-  Shield,
-  Waves,
-  Sparkles,
-} from 'lucide-react'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-
   return (
-   
-    <div>Hello</div>
+    <div className="min-h-screen flex items-center justify-center bg-background p-4">
+      Hello
+    </div>
   )
 }
